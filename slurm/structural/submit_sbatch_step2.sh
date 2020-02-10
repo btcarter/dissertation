50c5f5003133306aa323e149d751ae969a01cf4f
btcarter <benjamin.t.carter@gmail.com>
2020-02-10 14:07:27 -0700

    updating variables

--- slurm/structural/submit_sbatch_step2.sh
+++ slurm/structural/submit_sbatch_step2.sh
@@ -10,10 +10,10 @@
 # --- VARIABLES --- #
 #####################
 
-STUDY=~/compute/skilledReadingStudy					    # location of study directory
+STUDY=~/compute/NihReadingStudy					    # location of study directory
 TEMPLATE_DIR=${STUDY}/template 							# destination for template output
-SCRIPT_DIR=~/analyses/structuralSkilledReading			# location of scripts that might be referenced; assumed to be separate from the data directory.
-LIST=${SCRIPT_DIR}/participants.tsv 					# list of participant IDs
+SCRIPT_DIR=~/analyses/dissertation/structural			# location of scripts that might be referenced; assumed to be separate from the data directory.
+LIST=${STUDY}/dissertation/participants.tsv 					# list of participant IDs
 LOG=~/logfiles											# where to put documentation about errors and outputs
 TIME=`date '+%Y_%m_%d-%H_%M_%S'`						# time stamp for e's and o's
 
@@ -31,7 +31,7 @@ fi
 
 # submit the job script once per participant
 for i in $(cat $LIST); do
-	
+
     sbatch \
     -o ${OUT}/output_step2_${i}.txt \
     -e ${OUT}/error_step2_${i}.txt \
