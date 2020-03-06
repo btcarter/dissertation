3d81a8c8be92b9339f145625fe25b484bc745360
btcarter <benjamin.t.carter@gmail.com>
2020-03-06 16:08:12 -0700

    trying to get this to work

--- local/roiMasks/loadDataset.py
+++ local/roiMasks/loadDataset.py
@@ -1,20 +1,24 @@
 #!/bin/python
 
 # this script will load the dataset to be used for mask creation from Neurosynth. This should only be run once in a great while.
+
+import os
+
+os.chdir(r"/Users/ben88/Downloads/ns")
+
+import neurosynth as ns
+ns.dataset.download(path="/Users/ben88/Downloads/ns", unpack=True)
+
 # Core functionality for managing and accessing data
 from neurosynth import Dataset
 # Analysis tools for meta-analysis, image decoding, and coactivation analysis
 from neurosynth import meta, decode, network
 
-# set directory
-path="~/Box/LukeLab/NIH Dyslexia Study/data/neurosynth/"
-os.chdir(path)
-
 # Downloand datatset for mask creation
-    # Create a new Dataset instance
-    dataset = Dataset('data/database.txt')
+# Create a new Dataset instance
+dataset = Dataset('database.txt')
 
-    # Add some features
-    dataset.add_features('data/features.txt')
+# Add some features
+dataset.add_features('features.txt')
 
-    dataset.save('dataset.pkl')
+dataset.save('dataset.pkl')
