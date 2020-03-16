40663664dc3d0c1dd7e20c9eed1dd06b4d819c32
btcarter <benjamin.t.carter@gmail.com>
2020-03-16 16:23:26 -0600

    fixed?

--- slurm/fMRI/rvp/3dDeconvolve_rvp_job.sh
+++ slurm/fMRI/rvp/3dDeconvolve_rvp_job.sh
@@ -79,8 +79,8 @@ if [ -f $TIMING_POS ] && [ ! -f rvp_deconv+orig.BRIK ]
             -glt_label 1 READ \
 						-gltsym 'SYM: PIC' \
             -glt_label 2 PIC \
-						-gltsym 'SYM: READ-PIC' \
-						-glt_label 3 read -pic \
+						-gltsym 'SYM: READ -PIC' \
+						-glt_label 3 READ-PIC \
             -censor "$subj_DIR/motion/motion_censor_vector.txt[0]" \
             -nocout -tout \
             -bucket rvp_deconv \
