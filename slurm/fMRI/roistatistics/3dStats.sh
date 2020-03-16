0d219c1c50473a8b7a5b0b575221c1cf31375965
btcarter <benjamin.t.carter@gmail.com>
2020-03-16 15:30:57 -0600

    starting script for extracting participant roi stats

---
+++ slurm/fMRI/roistatistics/3dStats.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This script will harvest relevant stats for each roi per study participant and
+# put that in a table for you.
+# masks were created manually via 3dClusterize, for example:
+# 3dClusterize -nosum -1Dformat -inset
+# /Users/ben88/Box/LukeLab/NIH Dyslexia Study/data/masks/eye movements_association-test_z_FDR_0.01.nii
+# -idat 0 -ithr 0 -NN 1 -clust_nvox 40 -bisided -0.0001 0.0001 -pref_map om_mask
+
+
+START=$(pwd)    #starting directory
+PROJECT=/fslhome/ben88/compute/NihReadingStudy/   #home directory
+SUBJ_DIR=${PROJECT}/functional #directory with individual subject data
+RES_DIR=${PROJECT}/dissertation/roiStats  #directory with group results
+READING=${RES_DIR}/readingNetwork.txt   #statistics data
+OM=${RES_DIR}/omNetwork.txt
+STR=${RES_DIR}/striatum.txt
+MASK=${PROJECT}/masks/reading_mask+tlrc #mask used to create statistics
+DECON=block/block_deconv_blur5_ANTS_resampled+tlrc[1]  #path and prefix for participant's template aligned deconvolution file with subbrick
+PARTICIPANTS=${PROJECT}/dissertation/participants.tsv
+
+
+#make results directory and stats document
+
+if [ ! -d ${RES_DIR} ]
+  then mkdir ${RES_DIR}
+fi
+
+if [ ! -f ${OUTPUT} ]
+  then touch ${OUTPUT}
+fi
+
+for i in $(cat ${PARTICIPANTS}); do
+  stat="3dROIstats -minmax -sigma -1DRformat -mask ${MASK} ${SUBJ_DIR}/${i}/${DECON}"
+  ${stat} >> ${READING}
+  stat="3dROIstats -minmax -sigma -1DRformat -mask ${MASK} ${SUBJ_DIR}/${i}/${DECON}"
+  ${stat} >> ${OM}
+  stat="3dROIstats -minmax -sigma -1DRformat -mask ${MASK} ${SUBJ_DIR}/${i}/${DECON}"
+  ${stat} >> ${STR}
+done
