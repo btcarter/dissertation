c7530d740fb8389a5c5f2aa53d35793fe19d5358
btcarter <benjamin.t.carter@gmail.com>
2020-03-17 16:13:58 -0600

    going oldschool because the new 3dClusterize is finicky

--- slurm/fMRI/roistatistics/makeMasks.sh
+++ slurm/fMRI/roistatistics/makeMasks.sh
@@ -1,44 +1,72 @@
 #!/bin/bash
+MASTER=~/compute/NihReadingStudy/dissertation/predictablity/orthoWhole+tlrc
+RADIUS=7.5
 
-# this will create masks for 3droistatistics
+  # OM Mask
+  TABLE=~/compute/NihReadingStudy/masks/OM
 
-INPUT=~/compute/NihReadingStudy/masks/eye_movements_association-test_z_FDR_0.01.nii
-Z_brick=0
-T_brick=0
-neighbors=1
-CLUST=39
-left=-0.0001
-right=0.0001
-PREFIX=~/compute/NihReadingStudy/masks/ns_om_mask
+  # this will create masks for 3droistatistics
+  touch ${TABLE}.1D
 
-3dClusterize \
--nosum \
--1Dformat \
--inset ${INPUT} \
--idat ${Z_brick} \
--ithr ${T_brick} \
--NN ${neighbors} \
--clust_nvox ${CLUST} \
--bisided ${left} ${right} \
--pref_map ${PREFIX}
+  INPUT=~/compute/NihReadingStudy/masks/eye_movements_association-test_z_FDR_0.01.nii
+  Z_brick=0
+  T_brick=0
+  neighbors=1
+  CLUST=39
+  left=-0.0001
+  right=0.0001
+  PREFIX=~/compute/NihReadingStudy/masks/ns_om_mask
 
+  3dclust \
+  -1Dformat \
+  -orient LPI \
+  -1dindex ${T_brick} \
+  -1tindex ${T_brick} \
+  -2thresh ${left} ${right} \
+  -dxyz=1 \
+  -savemask ${PREFIX} \
+  1.01 \
+  ${CLUST} >> ${TABLE}.1D
 
-INPUT=~/compute/NihReadingStudy/masks/reading_association-test_z_FDR_0.01.nii
-Z_brick=0
-T_brick=0
-neighbors=1
-CLUST=39
-left=-0.0001
-right=0.0001
-PREFIX=~/compute/NihReadingStudy/masks/ns_reading_mask
+  #output coordinate data for most active voxels and center of mass
+  1dcat ${TABLE}.1D'[13..15]' > ${TABLE}_MI.1D
 
-3dClusterize \
--nosum \
--1Dformat \
--inset ${INPUT} \
--idat ${Z_brick} \
--ithr ${T_brick} \
--NN ${neighbors} \
--clust_nvox ${CLUST} \
--bisided ${left} ${right} \
--pref_map ${PREFIX}
+  #afni makes the spherical mask
+  3dUndump -prefix sphereROI_MI -master $MASTER -orient LPI -srad $RADIUS -xyz ${TABLE}_MI.1D
+
+  # reading mask
+
+  TABLE=~/compute/NihReadingStudy/masks/READ
+
+  # this will create masks for 3droistatistics
+  touch ${TABLE}.1D
+
+  INPUT=~/compute/NihReadingStudy/masks/reading_association-test_z_FDR_0.01.nii
+  Z_brick=0
+  T_brick=0
+  neighbors=1
+  CLUST=39
+  left=-0.0001
+  right=0.0001
+  PREFIX=~/compute/NihReadingStudy/masks/ns_reading_mask
+
+  3dclust \
+  -1Dformat \
+  -orient LPI \
+  -1dindex ${T_brick} \
+  -1tindex ${T_brick} \
+  -2thresh ${left} ${right} \
+  -dxyz=1 \
+  -savemask ${PREFIX} \
+  1.01 \
+  ${CLUST}
+
+  #make functional roi mask and summary table
+  touch ${TABLE}.1D
+  $FUNCTIONAL_MASK $GROUP_FILE >> $TABLE.1D
+
+  #output coordinate data for most active voxels and center of mass
+  1dcat ${TABLE}.1D'[13..15]' > ${TABLE}_MI.1D
+
+  #afni makes the spherical mask
+  3dUndump -prefix sphereROI_MI -master $MASTER -orient LPI -srad $RADIUS -xyz ${TABLE}_MI.1D
