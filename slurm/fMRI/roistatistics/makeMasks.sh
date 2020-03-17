f41d7e23c4c92f4398cd0647f8ba1baad254410e
btcarter <benjamin.t.carter@gmail.com>
2020-03-17 15:08:25 -0600

    updating to make masks from neurosynth maps

--- slurm/fMRI/roistatistics/makeMasks.sh
+++ slurm/fMRI/roistatistics/makeMasks.sh
@@ -2,14 +2,14 @@
 
 # this will create masks for 3droistatistics
 
-INPUT=~/compute/NihReadingStudy/dissertation/block/blockWhole+tlrc
-Z_brick=4
-T_brick=5
+INPUT=~/compute/NihReadingStudy/masks/eye_movements_association-test_z_FDR_0.01.nii
+Z_brick=0
+T_brick=0
 neighbors=1
 CLUST=39
-left=-3.2905
-right=3.2905
-PREFIX=~/compute/NihReadingStudy/masks/roistats_block_pos_mask
+left=-0.0001
+right=0.0001
+PREFIX=~/compute/NihReadingStudy/masks/ns_om_mask
 
 3dClusterize \
 -nosum \
@@ -19,5 +19,26 @@ PREFIX=~/compute/NihReadingStudy/masks/roistats_block_pos_mask
 -ithr ${T_brick} \
 -NN ${neighbors} \
 -clust_nvox ${CLUST} \
--1sided RIGHT_TAIL ${right} \
+-bisided ${left} ${right} \
+-pref_map ${PREFIX}
+
+
+INPUT=~/compute/NihReadingStudy/masks/reading_association-test_z_FDR_0.01.nii
+Z_brick=0
+T_brick=0
+neighbors=1
+CLUST=39
+left=-0.0001
+right=0.0001
+PREFIX=~/compute/NihReadingStudy/masks/ns_reading_mask
+
+3dClusterize \
+-nosum \
+-1Dformat \
+-inset ${INPUT} \
+-idat ${Z_brick} \
+-ithr ${T_brick} \
+-NN ${neighbors} \
+-clust_nvox ${CLUST} \
+-bisided ${left} ${right} \
 -pref_map ${PREFIX}
