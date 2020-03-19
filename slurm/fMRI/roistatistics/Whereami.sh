245375df4a7b83e6e477236c8675bdcb2bd55e27
btcarter <benjamin.t.carter@gmail.com>
2020-03-18 21:02:05 -0600

    updating with new masks

--- slurm/fMRI/roistatistics/Whereami.sh
+++ slurm/fMRI/roistatistics/Whereami.sh
@@ -2,7 +2,7 @@
 
 # this will use the afni atlases to tell you where the blobs are in the brain
 MASKDIR=~/compute/NihReadingStudy/masks
-MASKLIST=(ns_om_mask_sphereROI_MI+tlrc ns_reading_mask_sphereROI_MI+tlrc)
+MASKLIST=(ns_reading_func_mask+tlrc ns_om_func_mask+tlrc)
 
 cd $MASKDIR
 for i in ${MASKLIST[@]}; do
