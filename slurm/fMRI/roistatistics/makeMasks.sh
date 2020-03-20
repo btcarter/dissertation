87463c90586aaa72f1c4ef2940717d20e5e398dd
btcarter <benjamin.t.carter@gmail.com>
2020-03-20 12:21:14 -0600

    adding one more mask

--- slurm/fMRI/roistatistics/makeMasks.sh
+++ slurm/fMRI/roistatistics/makeMasks.sh
@@ -35,3 +35,17 @@ MASK_DIR=~/compute/NihReadingStudy/masks
 -clust_nvox 40 \
 -1sided RIGHT_TAIL 0.0001 \
 -pref_map ${MASK_DIR}/ns_om_func_mask
+
+3dfractionize \
+-template ${MASTER} \
+-input ${MASK_DIR}/prediction_association-test_z_FDR_0.01.nii \
+-prefix ${MASK_DIR}/prediction_association
+
+3dClusterize -nosum \
+-1Dformat \
+-inset ${MASK_DIR}/prediction_association+tlrc \
+-ithr 0 \
+-NN 1 \
+-clust_nvox 40 \
+-1sided RIGHT_TAIL 0.0001 \
+-pref_map ${MASK_DIR}/ns_pred_func_mask
