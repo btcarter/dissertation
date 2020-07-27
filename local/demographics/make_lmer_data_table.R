568f1ef5ef242db56a3222728e7bee12ae36e50a
Benjamin T. Carter <benjamin.t.carter@gmail.com>
2020-07-27 12:33:49 -0600

    this will create a data table to be copy and pasted into the 3dlmer.sh script for lmer'ing.

---
+++ local/demographics/make_lmer_data_table.R
@@ -0,0 +1,53 @@
+# This script will assemble a data table for use in 3dLMEr
+
+library(readxl)
+library(dplyr)
+
+PART_LIST <- file.path("C:", "Users", "CarteB", "Box", "LukeLab", "NIH Dyslexia Study", "data",
+"participants", "master.xlsx")
+
+df <- read_xlsx(PART_LIST) %>% 
+  select(
+    mriID,
+    group
+  )
+
+
+# expand table with participant file names
+df <- df %>% rbind(df)
+df$condition <- c(
+  rep("reading", 52),
+  rep("pictures", 52)
+)
+
+df <- df %>% 
+  mutate(
+    InputFile = if_else(
+      condition == "reading",
+      paste(
+        "${FUNC_DIR}/",
+        mriID,
+        "/rvp/rvp_deconv_blur5_ANTS_resampled+tlrc'[1]'",
+        sep = ""
+      ),
+      paste(
+        "${FUNC_DIR}/",
+        mriID,
+        "/rvp/rvp_deconv_blur5_ANTS_resampled+tlrc'[3]'",
+        sep = ""
+      )
+    )
+  ) %>% 
+  rename(
+    Subj = mriID
+  ) %>% 
+  arrange(
+    group,
+    Subj,
+    condition
+  )
+
+OUT <- file.path("C:", "Users", "CarteB", "Box", "LukeLab", "NIH Dyslexia Study", "data",
+                 "participants", "lmerTable.tsv")
+
+write.table(df, file = OUT, sep = "\t")
