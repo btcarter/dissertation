a9f1b4201af77e5ad22a0fabde2828eb9016e2f7
Benjamin T. Carter <benjamin.t.carter@gmail.com>
2020-08-03 14:25:27 -0600

    simplifying the first lmer analysis

--- local/demographics/make_lmer_data_table.R
+++ local/demographics/make_lmer_data_table.R
@@ -14,66 +14,75 @@ df <- read_xlsx(PART_LIST) %>%
 
 
 # expand table with participant file names
-df <- df %>% rbind(df) %>% rbind(df) %>% rbind(df)
-df$condition <- c(
-  rep("pos", 52),
-  rep("lsa", 52),
-  rep("ortho",52),
-  rep("pictures", 52)
-)
+# df <- df %>% rbind(df)
+# df$condition <- c(
+#   rep("ortho", 52)
+# )
 
 df <- df %>% 
   mutate(
-    InputFile = if_else(
-      condition == "pos",
-      paste(
-        "${FUNC_DIR}/",
-        mriID,
-        "/predictability/predictability_blur5_ANTS_resampled+tlrc'[3]'",
-        " ",
-        " \\",
-        sep = ""
-      ),
-      if_else(
-        condition == "lsa",
-        paste(
-          "${FUNC_DIR}/",
-          mriID,
-          "/predictability/predictability_blur5_ANTS_resampled+tlrc'[5]'",
-          " ",
-          " \\",
-          sep = ""
-        ),
-        if_else(
-          condition == "ortho",
-          paste(
-            "${FUNC_DIR}/",
-            mriID,
-            "/predictability/predictability_blur5_ANTS_resampled+tlrc'[7]'",
-            " ",
-            " \\",
-            sep = ""
-          ),
-          paste(
-            "${FUNC_DIR}/",
-            mriID,
-            "/predictability/predictability_blur5_ANTS_resampled+tlrc'[1]'",
-            " ",
-            " \\",
-            sep = ""
-          )
-        )
-      )
+    InputFile = paste(
+      "${FUNC_DIR}/",
+      mriID,
+      "/predictability/predictability_blur5_ANTS_resampled+tlrc'[7]'",
+      " ",
+      " \\",
+      sep = ""
     )
-  ) %>% 
-  rename(
-    Subj = mriID
-  ) %>% 
-  arrange(
-    group,
-    Subj,
-    condition
   )
+
+# df <- df %>%
+#   mutate(
+#     InputFile = if_else(
+#       condition == "pos",
+#       paste(
+#         "${FUNC_DIR}/",
+#         mriID,
+#         "/predictability/predictability_blur5_ANTS_resampled+tlrc'[3]'",
+#         " ",
+#         " \\",
+#         sep = ""
+#       ),
+#       if_else(
+#         condition == "lsa",
+#         paste(
+#           "${FUNC_DIR}/",
+#           mriID,
+#           "/predictability/predictability_blur5_ANTS_resampled+tlrc'[5]'",
+#           " ",
+#           " \\",
+#           sep = ""
+#         ),
+#         if_else(
+#           condition == "ortho",
+#           paste(
+#             "${FUNC_DIR}/",
+#             mriID,
+#             "/predictability/predictability_blur5_ANTS_resampled+tlrc'[7]'",
+#             " ",
+#             " \\",
+#             sep = ""
+#           ),
+#           paste(
+#             "${FUNC_DIR}/",
+#             mriID,
+#             "/predictability/predictability_blur5_ANTS_resampled+tlrc'[1]'",
+#             " ",
+#             " \\",
+#             sep = ""
+#           )
+#         )
+#       )
+#     )
+#   ) %>% 
+#   rename(
+#     Subj = mriID
+#   ) %>% 
+#   arrange(
+#     group,
+#     Subj,
+#     condition
+#   )
 
 OUT <- file.path("C:", "Users", "CarteB", "Box", "LukeLab", "NIH Dyslexia Study", "data",
                  "participants", "lmerTable.tsv")
