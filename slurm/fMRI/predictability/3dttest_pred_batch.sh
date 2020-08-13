d98479603d2cc555265d691174c1781eb0214c48
Benjamin T. Carter <benjamin.t.carter@gmail.com>
2020-08-13 13:33:56 -0600

    running a Ttest per Gang's recommendation. Will LMER for trials

--- slurm/fMRI/predictability/3dttest_pred_batch.sh
+++ slurm/fMRI/predictability/3dttest_pred_batch.sh
@@ -27,21 +27,3 @@ sbatch \
     -e ~/logfiles/${var}/error_group_predWhole.txt \
     ${SCRIPT_DIR}/3dttest_predMaskWhole_job.sh \
     sleep 1
-
-sbatch \
-    -o ~/logfiles/${var}/output_group_predRead.txt \
-    -e ~/logfiles/${var}/error_group_predRead.txt \
-    ${SCRIPT_DIR}/3dttest_predMaskRead_job.sh \
-    sleep 1
-
-sbatch \
-    -o ~/logfiles/${var}/output_group_predOM.txt \
-    -e ~/logfiles/${var}/error_group_predOM.txt \
-    ${SCRIPT_DIR}/3dttest_predMaskOM_job.sh \
-    sleep 1
-
-sbatch \
-    -o ~/logfiles/${var}/output_group_predSTRI.txt \
-    -e ~/logfiles/${var}/error_group_predSTRI.txt \
-    ${SCRIPT_DIR}/3dttest_predMaskSTRI_job.sh \
-    sleep 1
